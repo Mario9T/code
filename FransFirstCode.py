a43722527aae492652083a5c4d9def6b4fcb976e
Mario9T <mario98issa@gmail.com>
2024-10-29 12:37:29 +0100

    First comment

--- FransFirstCode.py
+++ FransFirstCode.py
@@ -2,17 +2,15 @@
 namn = "Ditt Namn"  
 alder = 25          
 temperatur = 18.5   
-ar_sant = True     
-
+ar_sant = True      
 print("Namn:", namn, "| Typ:", type(namn))
 print("Ålder:", alder, "| Typ:", type(alder))
 print("Temperatur:", temperatur, "| Typ:", type(temperatur))
 print("Boolean-värde:", ar_sant, "| Typ:", type(ar_sant))
 
-# Uppgift 5: 
+# Uppgift 5:
 a = 15
 b = 4
-
 print("Addition (a + b):", a + b)
 print("Subtraktion (a - b):", a - b)
 print("Multiplikation (a * b):", a * b)
@@ -21,7 +19,7 @@ print("Golvdivision (a // b):", a // b)
 print("Modulus (a % b):", a % b)
 print("Exponentiering (a ** b):", a ** b)
 
-# Uppgift 6: 
+# Uppgift 6:
 text = "Hello, World!"
 print("Första tecknet:", text[0])
 print("Tecknen från position 7 till 12:", text[7:12])
